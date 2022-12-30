9b2db4bef204a0b466f10e2f159068c35bf954f4
Даниил <geimer_cok@mail.ru>
2022-12-30 17:29:05 +0300

    pause

--- MegTask/Program.cs
+++ MegTask/Program.cs
@@ -1,1 +1,24 @@
 ﻿Console.Clear();
+
+int EnterNumberN(string massege)
+{
+    System.Console.Write(massege);
+    int numberN = Int32.Parse(Console.ReadLine());
+    return numberN;
+}
+
+int n = EnterNumberN("Enter of number array");
+
+int[] ArrayLong = new int [n];
+{
+
+
+
+    for(int i =0: i<= n; i++)
+    {
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+ AbandonedMutexException  
