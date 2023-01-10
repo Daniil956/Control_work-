bd9e8165eb54afb34a7dd44ece32987c899a44fd
Даниил <geimer_cok@mail.ru>
2023-01-10 20:06:22 +0300

    Finish
    qw
    sdf
    
    asdfadq
    
    qq
    qw
    af
    qaaa

--- MegTask/Program.cs
+++ MegTask/Program.cs
@@ -1,9 +1,29 @@
 ﻿Console.Clear();
 
-string[] EnterNumberN()
+string[] EnterArrey(string message)
 {
-    System.Console.Write("Enter world of space: ");
+    System.Console.Write(message);
     string[] numberN = Console.ReadLine().Split(" ");
     return numberN;
 }
 
+string[] massiv = EnterArrey("Enter world of space: ");
+int count = 0;
+
+for (int i = 0; i < massiv.Length; i++)
+{
+    if (massiv[i].Length <= 3) count++ ;
+}
+
+string[] array = new string [count];
+int index =0;
+
+for(int i = 0; i< massiv.Length; i++)
+{
+    if(massiv[i].Length <= 3) 
+    {
+        array[index] = massiv[i]; 
+        index++;
+    }   
+}
+ System.Console.WriteLine(string.Join(", " , array) + " ");
