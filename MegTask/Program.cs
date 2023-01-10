0adb69b07d0d2e70ffb33f832298a1e804ad92a9
Даниил <geimer_cok@mail.ru>
2023-01-10 18:54:21 +0300

    Enter date in massiv

--- MegTask/Program.cs
+++ MegTask/Program.cs
@@ -1,24 +1,9 @@
 ﻿Console.Clear();
 
-int EnterNumberN(string massege)
+string[] EnterNumberN()
 {
-    System.Console.Write(massege);
-    int numberN = Int32.Parse(Console.ReadLine());
+    System.Console.Write("Enter world of space: ");
+    string[] numberN = Console.ReadLine().Split(" ");
     return numberN;
 }
 
-int n = EnterNumberN("Enter of number array");
-
-int[] ArrayLong = new int [n];
-{
-
-
-
-    for(int i =0: i<= n; i++)
-    {
-        System.Console.WriteLine();
-    }
-    System.Console.WriteLine();
-}
-
- AbandonedMutexException  
